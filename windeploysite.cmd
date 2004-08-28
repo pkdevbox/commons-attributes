f91038b4cb16b965bb85db8517c2864e40246e00
Leo Sutic <leosutic@apache.org>
2004-08-28 21:50:26 +0000

    Fixed windows deployment of website.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/attributes/trunk@141558 13f79535-47bb-0310-9956-ffa450edef68

--- windeploysite.cmd
+++ windeploysite.cmd
@@ -2,6 +2,7 @@
 
 if "%1" == "" goto :HELP
 
+call maven attributes:windeploysite
 pscp target/site.zip %1@cvs.apache.org:__site.zip
 plink %1@cvs.apache.org "rm -rf /www/jakarta.apache.org/commons/attributes/*;unzip __site.zip -d /www/jakarta.apache.org/commons/attributes/;rm -f __site.zip"
 
